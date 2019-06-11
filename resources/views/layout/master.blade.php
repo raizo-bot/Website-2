87faa1ec5895a5fd6f91610530f3fd756b8f671d
BasketBandit <errorsfound@hotmail.co.jp>
2019-06-11 10:12:12 +0100

    added basic css for navigation and jumbotron

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <title>@yield('title', 'Yuuko')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
 </head>
 
 <body>
@@ -13,8 +14,9 @@
         <span>Listening to {{ count($guilds) }} guilds and counting!
     </div>
 
-        <div class="container inherit light">
-            <ul class="nav navbar-fixed-top nav-tabs">
+    <div id="main-nav">
+        <div class="container">
+            <ul class="nav navbar-dark bg-dark">
                 <li class="nav-item">
                     <a class="nav-link" href="/">Commands</a>
                 </li>
@@ -40,6 +42,7 @@
                 </li>
             </ul>
         </div>
+    </div>
 
     @yield('content')
 </body>
