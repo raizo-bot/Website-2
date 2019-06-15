37cf0ea6c1ef62c207e24f7edbe430cbfe5c555a
BasketBandit <errorsfound@hotmail.co.jp>
2019-06-15 16:11:34 +0100

    added basic chart into metrics as a test

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -3,21 +3,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use App\Guild;
 
 class MasterController extends Controller {
 
 	public function index() {
-		$guilds = \App\Guild::all()->map->name;
+		$guilds = Guild::all();
 		return view('index', compact('guilds'));
 	}
 
 	public function metrics() {
-		$guilds = \App\Guild::all()->map->name;
-		return view('metrics', compact('guilds'));
+		$guilds = Guild::all();
+        $guildRegions = DB::table('Guilds')->select('guildRegion as name', DB::raw('count(guildRegion) as count'))->groupBy('guildRegion')->orderByRaw('count(guildRegion) DESC')->get()->toArray();
+
+		return view('metrics', compact('guilds', 'guildRegions'));
 	}
 
 	public function tutorials() {
-		$guilds = \App\Guild::all()->map->name;
+		$guilds = Guild::all();
 		return view('tutorials', compact('guilds'));
 	}
 
--- resources/views/metrics.blade.php
+++ resources/views/metrics.blade.php
@@ -4,6 +4,45 @@
 
 @section('content')
     <div class="container">
-        <h1>Coming soon! :)</h1>
+        <h2>Coming soon! :)</h2>
+        <canvas id="GuildRegions"></canvas>
     </div>
 @endsection
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+<script>
+    var ctx = document.getElementById('GuildRegions').getContext('2d');
+    var chart = new Chart(ctx, {
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+    labels: [
+        @foreach ($guildRegions as $region)
+            '{{ $region->name }}',
+        @endforeach
+    ],
+    datasets: [{
+    label: 'Region Distribution',
+    backgroundColor: 'rgba(150, 0, 0, 0.7)',
+    data: [
+        @foreach ($guildRegions as $region)
+            '{{ $region->count }}',
+        @endforeach
+    ]
+    }]
+    },
+
+    options: {
+        title: {
+                display: true,
+                text: 'Region Distribution',
+                fontColor: '#cccccc'
+        },
+        legend: {
+                display: false
+        }
+    }
+    });
+</script>
