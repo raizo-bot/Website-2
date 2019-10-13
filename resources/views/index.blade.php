b0afdc8d9f07b512c5881051a1e3bb63c11de376
BasketBandit <errorsfound@hotmail.co.jp>
2019-10-13 15:12:58 +0100

    modified formatting of hero page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,8 @@
                 <h1>Yuuko</h1>
                 <span id="tag-line">
                     Watching <code>{{ number_format(count($guilds)) }}</code> guilds <br>
-                    with <code>{{ number_format($guildMembers->members) }}</code> users, <br>
-                    in <code>{{ number_format(count($guildRegions)) }}</code> regions.
+                    with <code>{{ number_format($guildMembers->members) }}</code> users <br>
+                    in <code>{{ number_format(count($guildRegions)) }}</code> regions
             </div>
         </div>
     </div>
